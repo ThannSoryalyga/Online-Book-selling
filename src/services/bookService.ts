ffe1b1a89c6305e9fa9cd57d3b7f6070778dce1a
Tith Sa <tith.sa@institute.pse.ngo>
2025-10-17 09:48:32 +0700

    CRUD book

--- src/services/bookService.ts
+++ src/services/bookService.ts
@@ -21,3 +21,109 @@ export const createBookService = async (
     };
   }
 };
+
+export const getBooksService = async (): Promise<BookResult> => {
+  try {
+    const books = await bookSchema.find();
+    return {
+      success: true,
+      data: books,
+      message: "Books fetched successfully.",
+    };
+  } catch (error) {
+    console.error("Error fetching books:", error);
+    return {
+      success: false,
+      data: null as any,
+      message: "Failed to fetch books.",
+    };
+  }
+};
+
+export const getBookByIdService = async (id: string): Promise<BookResult> => {
+  try {
+    const book = await bookSchema.findById(id);
+    if (!book) {
+      return {
+        success: false,
+        data: null as any,
+        message: "Book not found.",
+      };
+    }
+
+    return {
+      success: true,
+      data: book,
+      message: "Book fetched successfully.",
+    };
+  } catch (error) {
+    console.error("Error fetching book:", error);
+    return {
+      success: false,
+      data: null as any,
+      message: "Failed to fetch book.",
+    };
+  }
+};
+export const updateBookService = async (
+  id: string,
+  updateData: Partial<CreateBookInput>
+): Promise<BookResult> => {
+  try {
+    const updatedBook = await bookSchema.findByIdAndUpdate(id, updateData, {
+      new: true,
+    });
+    if (!updatedBook) {
+      return {
+        success: false,
+        data: null as any,
+        message: "Book not found.",
+      };
+    }
+    return {
+      success: true,
+      data: updatedBook,
+      message: "Book updated successfully.",
+    };
+  } catch (error) {
+    console.error("Error updating book:", error);
+    return {
+      success: false,
+      data: null as any,
+      message: "Failed to update book.",
+    };
+  }
+};
+
+export const deleteBookService = async (id: string): Promise<BookResult> => {
+  try {
+    const deletedBook = await bookSchema.findByIdAndDelete(id);
+    if (!deletedBook) {
+      return {
+        success: false,
+        data: null as any,
+        message: "Book not found.",
+      };
+    }
+    return {
+      success: true,
+      data: deletedBook,
+      message: "Book deleted successfully.",
+    };
+  } catch (error) {
+    console.error("Error deleting book:", error);
+    return {
+      success: false,
+      data: null as any,
+      message: "Failed to delete book.",
+    };
+  }
+};
+
+export default {
+  createBookService,
+  getBooksService,
+  getBookByIdService,
+  updateBookService,
+  deleteBookService,
+};
